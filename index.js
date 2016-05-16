23b785da01daf05f0b61778812beb37a1441686d
grapaa <paalgraf@gmail.com>
2016-05-16 21:54:17 +0200

    compile

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-(function() {
+module.exports = bngt = (function () {
+
+
+    bngt.prototype.type = 'template';
+
+    bngt.prototype.extension = 'html';
     
-    
-    
-    
-    
+    bngt.prototype.compile = function(data, path, callback) {
+    var html, url;
+    html = escape(data);
+    console.log(html);
+  };
+
+
 })();
+
